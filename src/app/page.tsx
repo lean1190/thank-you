2a09dca84a3c03ba7a4ba48ee16b5a5bad0fb602
Lean <leian1306@gmail.com>
2024-02-24 17:18:57 +0100

    home: move the behavior to a custom hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,18 @@
 'use client';
 
-import { useRef } from 'react';
+import { RefObject, useRef } from 'react';
 import { useInterval } from 'usehooks-ts';
 
 import styles from './styles.module.css';
 
-const words = [
-    'Thank you for trusting me with your learning',
-    'I\'m glad we met',
-    'I hope you learned something valuable',
-    'Let\'s keep learning together',
-    'Looking forward to helping you again'
-];
-
-let part;
-let i = 0;
-let offset = 0;
-const len = words.length;
-let forwards = true;
-let skipCount = 0;
-const skipDelay = 15;
-
-export default function Home() {
-    const wordRef = useRef<HTMLInputElement>(null);
+function useTypedWords(words: string[], elementRef: RefObject<HTMLElement>) {
+    let part;
+    let i = 0;
+    let offset = 0;
+    const len = words.length;
+    let forwards = true;
+    let skipCount = 0;
+    const skipDelay = 15;
 
     useInterval(() => {
         if (forwards) {
@@ -52,15 +42,28 @@ export default function Home() {
             }
         }
 
-        if (wordRef?.current) {
-            wordRef.current.innerText = part;
+        if (elementRef?.current) {
+            elementRef.current.innerText = part;
         }
     }, 70);
+}
+
+const appreciationWords = [
+    'Thank you for trusting me with your learning',
+    'I\'m glad we met',
+    'I hope you learned something valuable',
+    'Let\'s keep learning together',
+    'Looking forward to helping you again'
+];
+
+export default function Home() {
+    const elementRef = useRef<HTMLHeadingElement>(null);
+    useTypedWords(appreciationWords, elementRef);
 
     return (
         <main className="w-full p-4 text-left lg:w-[1000px]">
             <h1 className={`${styles.hi} mb-10 w-80 px-4`}>Hi there!</h1>
-            <h2 className={`${styles.word} h-12 text-neutral-950`} ref={wordRef}></h2>
+            <h2 className={`${styles.word} h-12 text-neutral-950`} ref={elementRef}></h2>
         </main>
     );
 }
