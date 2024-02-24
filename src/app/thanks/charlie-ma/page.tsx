0b1c53266346675ee314ebc9d8fcbc907b057908
Lean <leian1306@gmail.com>
2024-02-24 17:19:56 +0100

    thanks: make the background more pleasant to the eye

--- src/app/thanks/charlie-ma/page.tsx
+++ src/app/thanks/charlie-ma/page.tsx
@@ -3,7 +3,7 @@ import Card from '@/app/components/card';
 export default function CharlieMa() {
     return <Card
         hi="Hi Charlie!"
-        backgroundClasses="bg-gradient-to-b from-purple-300 to-pink-300"
+        backgroundClasses="bg-gradient-to-b from-purple-100 to-pink-300"
         paragraphs={[
             'Thank you very much for trusting me, and allowing me to help you grow your coding skills.',
             'Let\'s get started!'
